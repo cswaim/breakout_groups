ef9f1f0c74d160e02c9cd2f1910d9c889188659a
Criss Swaim <cswaim@jcrl.net>
2023-10-03 08:38:20 -0600

    add event data to run stat csv (#78)

--- src/reports_run_stats.py
+++ src/reports_run_stats.py
@@ -165,9 +165,9 @@ class RunStats():
 
     def write_stats_csv(self,):
         """write stats to csv"""
-        headers="Date/Time, Interactions, Missed_Interactions, Duplicate_Interactions, Interaction_Ratio, Unique_Interactions, Interaction_Ratio_Unique, Missed_Interactions, Event_Possible_Unique_Interactions, Max_Possible_Unique_Interactions, Max_divi, Possible_Group_Combinations, Group_Combinations\n"
+        headers="Date/Time, Interactions, Algorithm, Missed_Interactions, Duplicate_Interactions, Interaction_Ratio, Unique_Interactions, Interaction_Ratio_Unique, Missed_Interactions, Event_Possible_Unique_Interactions, Max_Possible_Unique_Interactions, Max_divi, Possible_Group_Combinations, Group_Combinations, Num_Attendees, Group_Size, Num_Groups, Num_Sessions \n"
 
-        dtl = f'"{datetime.now()}", {self.inter_cnt}, {self.miss_inter_cnt}, {self.dup_inter_cnt}, {self.inter_ratio_tot}, {self.unique_inter_cnt}, {self.inter_ratio_unique}, {self.miss_inter_cnt}, {self.pui}, {self.maxpui}, {self.maxidivi}, {self.puc}, {self.gc}\n'
+        dtl = f'"{datetime.now()}", {cfg.sys_group_algorithm_class}, {self.inter_cnt}, {self.miss_inter_cnt}, {self.dup_inter_cnt}, {self.inter_ratio_tot}, {self.unique_inter_cnt}, {self.inter_ratio_unique}, {self.miss_inter_cnt}, {self.pui}, {self.maxpui}, {self.maxidivi}, {self.puc}, {self.gc}, {cfg.n_attendees}, {cfg.group_size}, {cfg.n_groups}, {cfg.n_sessions}\n'
 
         csvfl_path = Path(f'{cfg.datadir}run_stats.csv')
 
