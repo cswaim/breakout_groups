a53688d06f68c6f91953a7e0998f7574f0744358
Criss Swaim <cswaim@jcrl.net>
2023-10-20 11:42:32 -0600

    mod set index

--- src/plot_algo_compare.py
+++ src/plot_algo_compare.py
@@ -37,13 +37,17 @@ class PlotAlgoCompare():
         plot_pdf_path = Path(f'{cfg.datadir}plot_rs{plot_id}.pdf')
         return plot_pdf_path
 
+    def set_df_index(self,):
+        """ set the dataframe x index to date-time column"""
+        # define the index column (x axis)
+        self.df.set_index('Date/Time',inplace=True)
+
     def plot_unique_interactions(self, ):
         """ plot the unique interactions for each run"""
         #plot_path = self.build_plot_file_name(plot_id="ui")
         uiplot = plt.figure("ui")
         uiplot.suptitle("Unique Interactions")
-        # define the index column (x axis)
-        self.df.set_index('Date/Time',inplace=True)
+
         # group by alogrithm
         self.df.groupby('Algorithm')['Unique_Interactions'].plot(legend=True)
 
@@ -55,8 +59,7 @@ class PlotAlgoCompare():
         """ plot the missed interactions for each run"""
         miplot = plt.figure("mi")
         miplot.suptitle("Missed Interactions")
-        # define the index column (x axis)
-        #self.df.set_index('Date/Time',inplace=True)
+
         # group by alogrithm
         self.df.groupby('Algorithm')['Missed_Interactions'].plot(legend=True)
 
@@ -79,8 +82,7 @@ class PlotAlgoCompare():
         plotpdf = self.create_pdf_obj(plot_id="rt")
         rtplot = plt.figure("rt")
         rtplot.suptitle("Algorithm Run Time")
-        # define the index column (x axis)
-        #self.df.set_index('Date/Time',inplace=True)
+
         # group by alogrithm
         self.df.groupby('Algorithm')['Algorithm_Runtime'].plot(legend=True)
 
@@ -92,7 +94,8 @@ class PlotAlgoCompare():
         # build the full path to csv file
         csvfl_path = Path(f'{cfg.datadir}run_stats.csv')
         self.df = pd.read_csv(csvfl_path)
-        # print(self.df)
+        self.set_df_index()
+        print(self.df)
         self.pp = self.create_pdf_obj()
 
         self.plot_unique_interactions()
