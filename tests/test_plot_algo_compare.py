0b793e57562d508fb4afe1c0f6aadccd158b810d
Criss Swaim <cswaim@jcrl.net>
2023-10-24 16:26:34 -0600

    modify path test

--- tests/test_plot_algo_compare.py
+++ tests/test_plot_algo_compare.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 import datetime
-import pathlib
+from pathlib import Path
 import src.config as cfg
 from src.plot_algo_compare import PlotAlgoCompare
 import pytest
@@ -10,8 +10,8 @@ import pytest
 
 def test_build_plot_file_name(config_event_defaults):
     """test building of all card interactions"""
-    rsp1 = pathlib.WindowsPath(f'{cfg.datadir}plot_rs.pdf')
-    rsp2 = pathlib.WindowsPath(f'{cfg.datadir}plot_rs_testplot.pdf')
+    rsp1 = Path(f'{cfg.datadir}plot_rs.pdf')
+    rsp2 = Path(f'{cfg.datadir}plot_rs_testplot.pdf')
     pac = PlotAlgoCompare()
     pac_pfn = pac.build_plot_file_name()
     assert rsp1 == pac_pfn
