5ec5cb12d752063d98fd08fe86be50b22fb073ba
Criss Swaim <cswaim@jcrl.net>
2024-02-08 11:41:44 -0700

    add tests for reports_cards_extra

--- tests/test_report_cards_extra.py
+++ tests/test_report_cards_extra.py
@@ -32,13 +32,21 @@ def test_update_sess_group(config_event_defaults, create_folders):
     rce = ReportsCardsExtra()
     rce.update_sess_group(0, 2, 1)
 
-    assert rce.sessions[0][2] == [1, 2, 7, 11]
-    assert rce.extra_sess[0][2] == [11]
+    assert rce.sessions[2][1] == [1, 2, 7, 11]
+    assert rce.extra_sess[2][1] == [11]
 
 def test_add_extra_cards_to_group(config_event_defaults, create_folders):
     """test add card to group"""
     rce = ReportsCardsExtra()
     rce.add_extra_cards_to_group()
+    print(rce.sessions)
+    print(rce.extra_sess)
 
+    assert rce.sessions[0][1] == [0, 5, 6, 11]
     assert rce.sessions[1][0] == [7, 8, 10, 11, 12]
-    assert rce.extra_sess[0][2] == [11]
+    assert rce.sessions[2][1] == [1, 2, 7, 11]
+    assert rce.sessions[3][1] == [0, 1, 7, 11]
+    assert rce.extra_sess[0][1] == [11]
+    assert rce.extra_sess[1][0] == [11, 12]
+    assert rce.extra_sess[2][1] == [11]
+    assert rce.extra_sess[3][1] == [11]
