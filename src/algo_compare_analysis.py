54fd7f8b4112042da30f02009d020fca50c2a5a8
Criss Swaim <cswaim@jcrl.net>
2024-02-23 17:49:24 -0700

    correct output of compare run

--- src/algo_compare_analysis.py
+++ src/algo_compare_analysis.py
@@ -23,19 +23,19 @@ class AlgoCompareAnalysis():
 
     def build_dataframe(self,):
         """build the panadas dataframe from the run stats csv"""
-        custom_hd = ["Date/Time","algorithm","algo_rt","uniq_i","mis_i","dup_i","rti","rui","pui","max_pi","max_indi","puc","tot_comb","n_attendees","group_size","mgroup_size","n_groups","n_sessions","n_extra_cards","rand_seed"]
         csvfl_path = Path(f'{cfg.datadir}{cfg.sys_run_stats_csv}')
-        # self.df = pd.read_csv(csvfl_path)
-        self.df = pd.read_csv(csvfl_path, header=None, names=custom_hd, skiprows=1)
+
+        # load csv and use custom column names by renaming columns
+        # rename continues to work as ne columns are added to csv
+        custom_cols = {"Algorithm": "algorithm", "Algorithm_RunDur": "algo_rt", "Unique_Interactions": "uniq_i", "Missed_Interactions": "mis_i",  "Duplicate_Interactions": "dup_i", "Ratio_Interactions": "rti", "Ratio_Unique_Interactions": "rui", "Event_Possible_Unique_Interactions": "pui", "Max_Interactions": "max_pi", "Max_IndivInt": "max_indi", "Possible_Group_Combinations": "puc", "Group_Combinations": "tot_comb", "Num_Attendees": "n_attendees", "Group_Size": "group_size", "Max_Group_Size": "mgroup_size", "Num_Groups": "n_groups", "Num_Sessions": "n_sessions", "Num_Extra_Cards": "n_extra_cards", "Random_Seed": "rand_seed"}
+        self.df = pd.read_csv(csvfl_path)
+        self.df.rename(custom_cols, axis='columns', inplace=True)
 
     def find_best_ratio(self,):
         """ sort the df by unique interactions ratio
             select the top 3 for each module
             return answer set df
         """
-        # Date/Time,Algorithm,Algorithm_Runtime,Interactions,Missed_Interactions,Duplicate_Interactions,Interaction_Ratio,Unique_Interactions,Interaction_Ratio_Unique,Event_Possible_Unique_Interactions,Max_Possible_Unique_Interactions,Max_idivi,Possible_Group_Combinations,Group_Combinations,Num_Attendees,Group_Size,Num_Groups,Num_Sessions,Random_Seed
-
-        #headers="Date/Time,algorithm,algo_rt,tot_i,mis_i,dup_i,rti,uniq_i,rui,pui,max_pui,max_indi,puc,tot_comb,n_attendees,group_size,mgroup_size,n_groups,n_sessions,r_seed\n"
 
         # get the top 3 for each algorithm
         df_res = self.df.sort_values(by=['algorithm', 'rui'],
@@ -53,9 +53,13 @@ class AlgoCompareAnalysis():
         print(f"         Max Group Size Occurence per Session: {estats['max_group_size_occurence']}")
         print(f"              Event Total Unique Interactions: {estats['pui']}")
         print(f"                       Event Max Interactions: {estats['max_i']}")
+        print("")
         print("  rui is Unique Interactions/ Event Total Unique Interactions")
         print("  rti is Unique Interactions/ Event Max Interactions")
-        print("")
+
+
+        # set max column width
+        pd.set_option('display.max_colwidth', 20)
 
         print(self.df_res[['algorithm', 'uniq_i', 'mis_i', 'dup_i', 'rti', 'rui', 'rand_seed']])
 
