16279b1db943e96d4ab485635a13970e32e87b94
Criss Swaim <cswaim@jcrl.net>
2024-03-01 16:01:50 -0700

    set group size

--- breakout_groups.py
+++ breakout_groups.py
@@ -31,6 +31,7 @@ class BreakoutGroups():
     def __init__(self, ) -> None:
         """setup"""
         self.n_attendees = cfg.n_attendees
+
         self.n_groups = cfg.n_groups
         self.n_sessions = cfg.n_sessions
         self.attendees_list = cfg.attendees_list
@@ -38,7 +39,10 @@ class BreakoutGroups():
         self.seed = self.gen_seed()
 
         # set group size
-        cfg.group_size = floor(cfg.n_attendees / cfg.n_groups)
+        if cfg.group_size > 0:
+            self.group_size = cfg.group_size
+        else:
+            cfg.group_size = floor(cfg.n_attendees / cfg.n_groups)
 
         logger_setup.run()
         log.info("beg breakout-groups")
